cd19d2434fefabe3629655bd11c5884c9e3f9b73
Tianlang Gu <78072288+harrisgugu@users.noreply.github.com>
2022-04-20 22:49:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-let firstcard = 2, secondcard = 3
-let cards = [firstcard,secondcard]
-let sum = firstcard + secondcard
-let age = 21
+let sum=  0
+let cards=  []
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEL = document.querySelector("#cards-el")
 
+let player = {
+    name:"Harris",
+    chips:135
+}
+
 function renderGame(){
     for(let i = 0;i<cards.length;i++){
-        cardsEL.innerText+=cards[i]+' '
+        cardsEL.innerText+=cards[i]+" "
     }
-    cardsEL.innerText = "Cards:" + firstcard +" "+secondcard  
     if(sum===21){
         message = "congrats! You have won Blackjack!"
         hasBlackJack = true
@@ -29,13 +31,39 @@ function renderGame(){
 }
 
 function startGame(){
+    let firstcard = getRandomCard()
+    let secondcard = getRandomCard()
+    cards = [firstcard,secondcard]
+    sum = firstcard + secondcard
     renderGame()
 }
 
 function newCard(){
-    let newCard = 10;
-    sum+=newCard;
-    renderGame();
+    if(isAlive&&hasBlackJack===false){
+        let newCard = getRandomCard;
+        sum+=newCard;
+        cards.push(newCard)
+        renderGame();
+    }
+    
+}
+
+function getRandomCard(){
+    let card =  Math.random()*13;
+    card = Math.floor(card)+1
+    if(card===1){
+        // let answer=""
+        // while(answer!=="1"||answer!=="11"){
+        //     answer = window.prompt("Would you like this 1 or 11?","1");
+        // }
+        // return parseInt(answer)
+        return 11        
+    }else if(card>10){
+        return 10
+    }
+    console.log(card)
+    return card
+    
 }
 
 
